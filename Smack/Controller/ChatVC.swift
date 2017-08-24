081f9db2328f6ad3821717d37636b29ba45b5a61
daver234 <dave.rothschild@gmail.com>
2017-08-23 20:06:12 -0700

    - set up storyboard - add ChannelVC and ChatVC - add SWRevealViewController from GitHub - Add Pods

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by David Rothschild on 8/23/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+}
