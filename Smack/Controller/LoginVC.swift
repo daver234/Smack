0dbbf8a6ac26899b97c297927469db9f91221de7
daver234 <dave.rothschild@gmail.com>
2017-08-24 19:34:16 -0700

    - add close button and method to LoginVC - minor adjustments

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by David Rothschild on 8/24/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBAction func closeTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
