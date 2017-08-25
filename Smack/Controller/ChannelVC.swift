69be8b6e1851342083cad5a9842cd4ed5f454a53
daver234 <dave.rothschild@gmail.com>
2017-08-24 19:10:36 -0700

    - build out Login view controller - add constants file - add constraints on ChannelVC

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    
+    
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by David Rothschild on 8/24/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
