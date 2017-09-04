a06ea3425ec43a15d1f3d4481ee8fe815b9f6a76
daver234 <dave.rothschild@gmail.com>
2017-09-03 23:22:00 -0700

    - carry avatar background color to sidebar using Scanner to parse RGB values

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -12,6 +12,7 @@ class ChannelVC: UIViewController {
 
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
         
@@ -23,5 +24,19 @@ class ChannelVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthoService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(componets: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
+    
 }
