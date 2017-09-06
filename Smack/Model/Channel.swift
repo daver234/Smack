4ea5a88d6dac12fa7d1d368d4e46890b5e2068d4
daver234 <dave.rothschild@gmail.com>
2017-09-05 22:44:42 -0700

    - added Message service and supporting struct to start the comms part

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by David Rothschild on 9/5/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
