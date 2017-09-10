3edaa8fd3083471206e0c7d55f35b0fe037f5317
daver234 <dave.rothschild@gmail.com>
2017-09-09 23:12:37 -0700

    - some clean up - get channel name to show up in main view controller - add notification so tableview of channel list reloads after adding channel - adjust login flow

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -15,6 +15,7 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
+    var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
         
@@ -40,7 +41,7 @@ class MessageService {
                         let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
                         self.channels.append(channel)
                     }
-                    print(self.channels)
+                    NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
                     completion(true)
                 }
             } else {
@@ -48,5 +49,9 @@ class MessageService {
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func clearChannels() {
+        channels.removeAll()
     }
 }
